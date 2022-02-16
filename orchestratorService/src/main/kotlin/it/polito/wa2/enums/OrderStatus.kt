9b9ca235b4a8ea116cf39a1da18be1d318f59085
luca.pezzolla <me@lucapezzolla.com>
2022-02-16 01:23:08 +0100

    fix: align OrderStatus enum values

--- orchestratorService/src/main/kotlin/it/polito/wa2/enums/OrderStatus.kt
+++ orchestratorService/src/main/kotlin/it/polito/wa2/enums/OrderStatus.kt
@@ -1,7 +1,10 @@
 package it.polito.wa2.enums
 
 enum class OrderStatus {
-    ORDER_CREATED,
-    ORDER_CANCELLED,
-    ORDER_COMPLETED
+    PENDING,
+    ISSUED,
+    DELIVERING,
+    DELIVERED,
+    FAILED,
+    CANCELED,
 }
